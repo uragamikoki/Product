fd5042abf0c5292f1d41a649679dfe9b4a689b30
uragamikoki <k-uragami@flight.co.jp>
2024-07-16 12:10:21 +0900

    Initial Commit

---
+++ Product/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Product
+//
+//  Created by Owner on 2024/07/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Product/ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductApp.swift
+//  Product
+//
+//  Created by Owner on 2024/07/16.
+//
+
+import SwiftUI
+
+@main
+struct ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
