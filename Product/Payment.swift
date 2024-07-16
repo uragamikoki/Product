d2601df3598dd7841e5d70d5ecf4bf5875859464
sho-kanai <s-kanai@flight.co.jp>
2024-07-16 16:35:37 +0900

    支払い選択ページ

---
+++ Product/Payment.swift
@@ -0,0 +1,67 @@
+//
+//  Payment.swift
+//  Product
+//
+//  Created by Owner on 2024/07/16.
+//
+
+import SwiftUI
+
+struct Payment: View {
+    var body: some View {
+        VStack{
+            Rectangle()
+                .frame(height: 0)
+                .opacity(0)
+            Text("支払い方法選択")
+                .font(.largeTitle)
+                .frame(width: 400,height: 80)
+                .background(.gray)
+                .foregroundColor(.white)
+            Spacer()
+            
+            RoundedRectangle(cornerRadius: 20.0)
+                .foregroundColor(.blue)
+                .frame(width: .infinity, height: 100)
+                .padding(.horizontal)
+                .overlay(Text("Paypal").font(.largeTitle).foregroundColor(.white))
+            
+            RoundedRectangle(cornerRadius: 20.0)
+                .foregroundColor(.blue)
+                .frame(width: .infinity, height: 100)
+                .padding(.horizontal)
+                .overlay(Text("クレジットカード").font(.largeTitle).foregroundColor(.white))
+            
+            RoundedRectangle(cornerRadius: 20.0)
+                .foregroundColor(.blue)
+                .frame(width: .infinity, height: 100)
+                .padding(.horizontal)
+                .overlay(Text("コンビニ支払い").font(.largeTitle).foregroundColor(.white))
+            
+            RoundedRectangle(cornerRadius: 20.0)
+                .foregroundColor(.blue)
+                .frame(width: .infinity, height: 100)
+                .padding(.horizontal)
+                .overlay(Text("Paypay").font(.largeTitle).foregroundColor(.white))
+            
+            RoundedRectangle(cornerRadius: 20.0)
+                .foregroundColor(.blue)
+                .frame(width: .infinity, height: 100)
+                .padding(.horizontal)
+                .overlay(Text("Apple Pay").font(.largeTitle).foregroundColor(.white))
+            Spacer()
+            Button(){
+                //back
+            }label: {
+                Text("戻る")
+                    .font(.title)
+            }
+
+        }
+        
+    }
+}
+
+#Preview {
+    Payment()
+}
