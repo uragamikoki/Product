11fcd57d606bbce6be4bc936cd6cd8c89e727d31
sho-kanai <s-kanai@flight.co.jp>
2024-07-16 15:25:15 +0900

    add

---
+++ Product/regist.swift
@@ -0,0 +1,18 @@
+//
+//  regist.swift
+//  Product
+//
+//  Created by Owner on 2024/07/16.
+//
+
+import SwiftUI
+
+struct regist: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    regist()
+}
